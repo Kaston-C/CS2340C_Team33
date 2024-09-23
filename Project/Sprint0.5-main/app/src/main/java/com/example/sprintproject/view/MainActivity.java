c4dc5fd28d773bc32292145fad755a22236ca28c
keithhb33 <keithburroughs33@gmail.com>
2024-09-23 14:40:25 -0400

    Remove old MainActivity.java

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -1,3 +1,14 @@
-public class MainActivity {
+package com.example.sprintproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
