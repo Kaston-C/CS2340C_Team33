2bc48166495b1c8caa484ee6f6d0e396350bde09
keithhb33 <keithburroughs33@gmail.com>
2024-09-30 13:15:41 -0400

    Remove redundant comment

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -8,7 +8,7 @@ import com.example.sprintproject.viewmodel.MainViewModel;
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mainViewModel;
-    private ActivityMainBinding binding;  // ViewBinding to access views in activity_main.xml
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
