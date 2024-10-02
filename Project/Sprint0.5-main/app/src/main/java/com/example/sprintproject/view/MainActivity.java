d4d95726e74ab8b2c463f4fb1e5860bfd1559c6f
Kaston-C <kaston_carr@MacBook-Air-15.local>
2024-10-01 23:57:06 -0400

    added navbar

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -8,7 +8,6 @@ import com.example.sprintproject.viewmodel.MainViewModel;
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mainViewModel;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
