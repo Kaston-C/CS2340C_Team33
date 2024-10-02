2b6029265947c8093b2684a3c53745dc9411f982
keithhb33 <keithburroughs33@gmail.com>
2024-10-01 21:14:57 -0400

    Add welcome logic to model, enable viewbinding, fixed error in view

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/MainModel.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/model/MainModel.java
@@ -1,3 +1,17 @@
-public class MainModel {
+package com.example.sprintproject.model;
 
+public class MainModel {
+    private String welcomeMessage;
+
+    public MainModel() {
+        this.welcomeMessage = "Welcome to WanderSync!";
+    }
+
+    public String getWelcomeMessage() {
+        return this.welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String message) {
+        this.welcomeMessage = message;
+    }
 }
--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sprintproject.view;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sprintproject.databinding.ActivityMainBinding;  // Check this import
 import com.example.sprintproject.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.onwardsButton.setOnClickListener(v -> {
+            mainViewModel.onStartButtonClick();
+        });
     }
 }
