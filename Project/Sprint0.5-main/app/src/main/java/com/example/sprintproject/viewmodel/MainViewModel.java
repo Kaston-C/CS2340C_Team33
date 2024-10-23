68131869c9bd7597e64c08801fe2572246fbbf8f
apilarczyk3 <apilarczyk3@gatech.edu>
2024-10-23 13:55:32 -0400

    Early Implementation of Calendar for DestinationActivity

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodel/MainViewModel.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodel/MainViewModel.java
@@ -8,6 +8,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Date;
+
 public class MainViewModel extends ViewModel {
 
     private final MutableLiveData<Boolean> isStartButtonClicked = new MutableLiveData<>();
@@ -31,5 +33,9 @@ public class MainViewModel extends ViewModel {
 
     public void onStartButtonClick() {
         isStartButtonClicked.setValue(true);
+    }
+
+    public void setDate(int Day, int Month, int Year) {
+
     }
 }
