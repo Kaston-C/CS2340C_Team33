e82763bc281d631c59d613a9406fe562f040ab15
keithhb33 <keithburroughs33@gmail.com>
2024-10-25 15:43:35 -0400

    Add updated SONAR commits

--- SOLID_GRASP/Project.java
+++ SOLID_GRASP/Project.java
@@ -1,14 +1,37 @@
-public class Project {
-    protected String name;
-    protected String description;
-    protected String startDate;
-    protected String endDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-    public Project (String name, String description, String startDate, String endDate) {
+public class Project {
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private List<Task> tasks;
+    private List<TeamMember> teamMembers;
+
+    public Project(String name, String description, Date startDate, Date endDate) {
         this.name = name;
         this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.tasks = new ArrayList<>();
+        this.teamMembers = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public void addTeamMember(TeamMember member) {
+        teamMembers.add(member);
+    }
+
+    public void removeTeamMember(TeamMember member) {
+        teamMembers.remove(member);
     }
 }
-
--- SOLID_GRASP/Task.java
+++ SOLID_GRASP/Task.java
@@ -1,15 +1,32 @@
-public class Task {
+import java.util.Date;
+
+public class Task implements TaskInterface {
     protected String title;
     protected String description;
-    protected String dueDate;
+    protected Date dueDate;
     protected String status;
     protected int priority;
 
-    public Task (String title, String description, String dueDate, String status, int priority) {
+    public Task(String title, String description, Date dueDate, String status, int priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.status = status;
+        this.priority = priority;
+    }
+
+    @Override
+    public void markAsComplete() {
+        this.status = "Complete";
+    }
+
+    @Override
+    public void markAsInProgress() {
+        this.status = "In Progress";
+    }
+
+    @Override
+    public void setPriority(int priority) {
         this.priority = priority;
     }
 }
