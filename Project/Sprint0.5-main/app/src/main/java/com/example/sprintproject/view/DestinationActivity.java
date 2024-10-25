51d15985dc13790af4e47fd5a997ae23a7bd3468
apilarczyk3 <apilarczyk3@gatech.edu>
2024-10-25 19:17:38 -0400

    Updated UI and Fixed Logic for 2 of 3 Fields Entered

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/DestinationActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/DestinationActivity.java
@@ -1,5 +1,8 @@
 package com.example.sprintproject.view;
 
+import static java.time.LocalDate.of;
+
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,12 +18,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sprintproject.R;
 
+import java.time.LocalDate;
 import java.util.Calendar;
 
 public class DestinationActivity extends AppCompatActivity {
 
-    int start_day, start_month, start_year, end_day, end_month, end_year, duration;
+    int duration;
+    String end_string, start_string;
+    Calendar startSel, endSel;
     boolean start_select, end_select, duration_select;
+    int end_doy, start_doy, end_year, start_year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +71,13 @@ public class DestinationActivity extends AppCompatActivity {
                 DatePickerDialog dateSelect = new DatePickerDialog(DestinationActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                        start_day = i2;
-                        start_month = i1;
-                        start_year = i;
+                        startSel = Calendar.getInstance();
+                        startSel.set(i, i1, i2);
+                        start_doy = startSel.get(Calendar.DAY_OF_YEAR);
+                        start_year = startSel.get(Calendar.YEAR);
+                        start_string = i1 + "/" + i2 + "/" + i;
                         start_select = true;
-                        startDate.setText(start_month + "/" + start_day + "/" + start_year);
+                        startDate.setText(start_string);
                     }
                 }, year, month, day);
                 dateSelect.show();
@@ -87,11 +96,13 @@ public class DestinationActivity extends AppCompatActivity {
                 DatePickerDialog dateSelect = new DatePickerDialog(DestinationActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                        end_day = i2;
-                        end_month = i1;
-                        end_year = i;
+                        endSel = Calendar.getInstance();
+                        endSel.set(i, i1, i2);
+                        end_doy = endSel.get(Calendar.DAY_OF_YEAR);
+                        end_year = endSel.get(Calendar.YEAR);
+                        end_string = i1 + "/" + i2 + "/" + i;
                         end_select = true;
-                        endDate.setText(end_month + "/" + end_day + "/" + end_year);
+                        endDate.setText(end_string);
                     }
                 }, year, month, day);
                 dateSelect.show();
@@ -104,9 +115,31 @@ public class DestinationActivity extends AppCompatActivity {
         submitDestination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                duration_select = !durationText.getText().toString().isBlank();
                 try {
+                    duration_select = !durationText.getText().toString().isBlank();
                     if (duration_select) duration = Integer.parseInt(durationText.getText().toString());
+                    if (duration_select && end_select && start_select) {
+                        Toast.makeText(DestinationActivity.this, "Something Went Wrong", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (end_select && start_select) {
+                            if ((end_year - start_year) * 365 + (end_doy - start_doy) < 0) {
+                                Toast.makeText(DestinationActivity.this, "End Date Before Start", Toast.LENGTH_SHORT).show();
+                            } else {
+                                duration = (end_year - start_year) * 365 + (end_doy - start_doy) + 1;
+                                durationText.setText(String.format("%d", duration));
+                            }
+                        } else if (duration_select && start_select) {
+                            endSel = (Calendar) startSel.clone();
+                            endSel.add(Calendar.DAY_OF_YEAR, duration - 1);
+                            end_string = endSel.get(Calendar.MONTH) + "/" + endSel.get(Calendar.DAY_OF_MONTH) + "/" + endSel.get(Calendar.YEAR);
+                            endDate.setText(end_string);
+                        } else if (duration_select && end_select) {
+                            startSel = (Calendar) endSel.clone();
+                            startSel.add(Calendar.DAY_OF_YEAR, -duration + 1);
+                            start_string = startSel.get(Calendar.MONTH) + "/" + startSel.get(Calendar.DAY_OF_MONTH) + "/" + startSel.get(Calendar.YEAR);
+                            startDate.setText(start_string);
+                        }
+                    }
                 } catch (NumberFormatException e) {
                     Toast.makeText(DestinationActivity.this, "Invalid Duration", Toast.LENGTH_SHORT).show();
                 }
