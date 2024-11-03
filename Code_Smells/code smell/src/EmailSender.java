ecfb14452043d7dae2350a805d2551c0c0557974
Cole Fitzhenry <cboomer51@gmail.com>
2024-11-03 17:48:06 -0500

    Changed EmailSender to remove the unnecessary method and instead to use the class. Deleted the GiftCardItem.java class and instead included it as an attribute in the Order class.

--- Code_Smells/code smell/src/EmailSender.java
+++ Code_Smells/code smell/src/EmailSender.java
@@ -1,8 +1,16 @@
+import java.util.logging.Logger;
 
 public class EmailSender {
-    public static void sendEmail(Customer customer, String subject, String message){
-        System.out.println("Email to: " + customer.getEmail());
-        System.out.println("Subject: " + subject);
-        System.out.println("Body: " + message);
+    private static final Logger logger = Logger.getLogger(EmailSender.class.getName());
+
+    public void sendEmail(Customer customer, String subject, String message) {
+        if (customer == null || subject == null || message == null) {
+            logger.severe("Customer, subject, and message must not be null");
+            return;
+        }
+
+        logger.info("Email to: " + customer.getEmail());
+        logger.info("Subject: " + subject);
+        logger.info("Body: " + message);
     }
 }
