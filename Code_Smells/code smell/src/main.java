a98ea11ada5ebaa2b30ff7904dea8a6f2bd48a32
Hawthorne Brown <Hawthorne.f.brown@gmail.com>
2024-11-03 23:41:37 -0500

    fixed improper name as well as replaced writeup 5

--- Code_Smells/code smell/src/main.java
+++ Code_Smells/code smell/src/main.java
@@ -2,17 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class main {
-	public static void main(String[] args) {
+	public main(String[] args) {
         Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
         Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
-        Item item3 = new GiftCardItem("Gift Card", 10, 1, DiscountType.AMOUNT, 0);
+        Item item3 = new Item("Gift Card", 10, 1, DiscountType.AMOUNT, 0);
 
         List<Item> items = new ArrayList<>();
         items.add(item1);
         items.add(item2);
         items.add(item3);
 
-        Order order = new Order(items, "John Doe", "johndoe@example.com");
+        Customer john = new Customer("John Doe", "johndoe@example.com");
+        Order order = new Order(items, john, true);
 
         System.out.println("Total Price: " + order.calculateTotalPrice());
 
@@ -20,5 +21,6 @@ public class main {
 
         order.printOrder();
     }
+
 }
 
