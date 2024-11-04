cd1ec27b24e356ebd0e2dc8b0355d1effb1f0103
Keith Burroughs <51885619+keithhb33@users.noreply.github.com>
2024-11-03 23:58:15 -0500

    fix main

--- Code_Smells/code smell/src/main.java
+++ Code_Smells/code smell/src/main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class main {
-	public main(String[] args) {
+	public static void main(String[] args) {
         Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
         Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
         Item item3 = new Item("Gift Card", 10, 1, DiscountType.AMOUNT, 0);
