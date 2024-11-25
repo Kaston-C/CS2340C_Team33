564c74bc89eb82c281c120c185508bc92f24e2e7
Cole Fitzhenry <cboomer51@gmail.com>
2024-11-24 22:55:42 -0500

    Fixed TravelTests and deleted KastonTests.

--- Project/Sprint0.5-main/app/src/test/java/com/example/sprintproject/TravelTests.java
+++ Project/Sprint0.5-main/app/src/test/java/com/example/sprintproject/TravelTests.java
@@ -6,14 +6,11 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class TravelTests {
 
-    
     @Test
     public void testingTravelPostDetails() {
-
-        TravelPost travelPost = new TravelPost("Spain", "2024-11-01", "2024-11-10", "Hotel", "Coffee Shop", "Train", "Fun!");
+        TravelPost travelPost = new TravelPost("user1", "Spain", "2024-11-01", "2024-11-10", "Hotel", "Coffee Shop", "Train", "Fun!");
         assertEquals("Spain", travelPost.getDestination());
         assertEquals("2024-11-01", travelPost.getStartDate());
         assertEquals("2024-11-10", travelPost.getEndDate());
@@ -23,14 +20,12 @@ public class TravelTests {
         assertEquals("Fun!", travelPost.getNotes());
     }
 
-
     @Test
     public void testingTravelListAdding() {
-
         List<TravelPost> travel = new ArrayList<>();
         assertEquals(0, travel.size());
 
-        TravelPost travels = new TravelPost("Spain", "2024-09-01", "2024-09-10", "Hotal", "Donut Shop", "Bus", "Fun Trip!");
+        TravelPost travels = new TravelPost("user1", "Spain", "2024-09-01", "2024-09-10", "Hotal", "Donut Shop", "Bus", "Fun Trip!");
         travel.add(travels);
 
         assertEquals(1, travel.size());
